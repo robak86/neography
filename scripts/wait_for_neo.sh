69f914e66e548b28b2aa36729a66b4332bac992f
Tomasz Robaczewski <robak86@gmail.com>
2017-04-22 17:25:53 +0200

    Move wait script into package.json

--- scripts/wait_for_neo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Waiting Neo4j to launch on 7474..."
-
-while ! curl --silent http://localhost:7474/; do
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
-
-echo "Neo4j launched"
