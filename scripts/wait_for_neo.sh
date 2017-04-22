1e09102d91859928491ce471b7ef01888aff5432
Tomasz Robaczewski <robak86@gmail.com>
2017-04-22 17:14:38 +0200

    Fix wait script

--- scripts/wait_for_neo.sh
+++ scripts/wait_for_neo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Waiting Neo4j to launch on 7687..."
+echo "Waiting Neo4j to launch on 7474..."
 
-while ! nc -z localhost 7687; do
+while ! curl --silent http://localhost:7474/; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
