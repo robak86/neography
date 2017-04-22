2b8e1d44ceb841eca6189322644634c1bcd016e5
Tomasz Robaczewski <robak86@gmail.com>
2017-04-22 14:34:36 +0200

    Add method overloading in order to stay type safe

--- lib/cypher/builders/MatchBuilder.ts
+++ lib/cypher/builders/MatchBuilder.ts
@@ -9,15 +9,22 @@ import {MatchedNodeQueryPart} from "../common/MatchedNodeQueryPart";
 import {INodeMatchQueryPart} from "../match/INodeMatchQueryPart";
 import {IRelationMatchQueryPart} from "../match/IRelationMatchQueryPart";
 import {Type} from "../../utils/types";
-
+import {isPresent} from "../../utils/core";
+import * as _ from 'lodash';
 
 export class MatchBuilder {
 
-    node<N extends AbstractNode>(klass?:Type<N>):INodeMatchQueryPart<N> {
-        if (klass) {
-            return new MatchNodeQueryPart<N>(klass);
+    node<N extends AbstractNode>(klass:Type<N>):INodeMatchQueryPart<N>
+    node<N extends AbstractNode>(klass?):INodeMatchQueryPart<any>
+    node<N extends AbstractNode>(klass?):any {
+        if (isPresent(klass) && !_.isFunction(klass)){
+            throw new Error(`Wrong class ${JSON.stringify(klass)}`);
+        }
+
+        if (isPresent(klass)) {
+            return new MatchNodeQueryPart<N>(klass as any);
         } else {
-            return new MatchUntypedNodeQueryPart<N>();
+            return new MatchUntypedNodeQueryPart<any>() as INodeMatchQueryPart<any>;
         }
     };
 
