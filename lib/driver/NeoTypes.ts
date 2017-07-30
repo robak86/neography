c75812a017c6010ea526e587c9afa5c57b1272ad
Tomasz Robaczewski <robak86@gmail.com>
2017-07-30 14:36:22 +0200

    Refactor type guards for Node and Relationships classes

--- lib/driver/NeoTypes.ts
+++ lib/driver/NeoTypes.ts
@@ -1,17 +1,13 @@
-
-
 import neo4j from "neo4j-driver";
-
-export const Neo4jNode = neo4j.types.Node;
-export const Neo4jRelationship = neo4j.types.Relationship;
-
-
 import {Node, Relationship} from "neo4j-driver/types/v1/graph-types";
 
+const Neo4jNode = neo4j.types.Node;
+const Neo4jRelationship = neo4j.types.Relationship;
+
 export function isNode(node: any): node is Node {
-    return (node && node.labels)
+    return node instanceof Neo4jNode;
 }
 
 export function isRelationship(rel: any): rel is Relationship {
-    return (rel && rel.type)
+    return rel instanceof Neo4jRelationship;
 }
