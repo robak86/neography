d4145ae6431569392abaf3c368d8916edd0295f8
Tomasz Robaczewski <robak86@gmail.com>
2017-12-27 15:32:17 +0100

    Add assertion for id property

--- lib/model/AbstractEntity.ts
+++ lib/model/AbstractEntity.ts
@@ -3,11 +3,14 @@ import {attribute} from "../annotations";
 import {AttributesMetadata} from "../metadata/AttributesMetadata";
 
 export abstract class AbstractEntity<T> {
-    @attribute() id?:string;
+    @attribute() readonly id?:string;
 
     constructor(params:Partial<T> = {}) {
-        let attributesMetadata = AttributesMetadata.getForInstance(this);
+        if (isPresent((params as any).id)) {
+            throw new Error("Params object passed to constructor cannot contain id property!")
+        }
 
+        let attributesMetadata = AttributesMetadata.getForInstance(this);
         Object.keys(params).forEach(propertyName => {
             if (!attributesMetadata.isAttributeDefined(propertyName)) {
                 console.warn(`${this.constructor.name} will be initialized with unknown param: ${propertyName}`)
