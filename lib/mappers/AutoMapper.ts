f8f1ec6238c433c0d3e27a3a3a1063535e53c9a9
Tomasz Robaczewski <robak86@gmail.com>
2017-12-28 19:36:25 +0100

    Add mapping for nested arrays

--- lib/mappers/AutoMapper.ts
+++ lib/mappers/AutoMapper.ts
@@ -2,6 +2,7 @@ import {AttributesMapperFactory} from "./AttributesMapperFactory";
 import Record from "neo4j-driver/types/v1/record";
 import {Node, Relationship} from "neo4j-driver/types/v1/graph-types";
 import {isNode, isRelationship} from "../driver/NeoTypes";
+import * as _ from 'lodash';
 
 
 export class AutoMapper {
@@ -11,21 +12,29 @@ export class AutoMapper {
         return records.map((record:Record) => {
             let row = {};
 
-            record.forEach((recordField:Node|Relationship, id) => {
-                if (isNode(recordField) && this.attributesMapperFactory.hasNodeMapper(recordField.labels)) {
-                    row[id] = this.attributesMapperFactory.getNodeAttributesMapper(recordField.labels).mapToInstance(recordField);
-                    return;
-                }
-
-                if (isRelationship(recordField) && this.attributesMapperFactory.hasRelationMapper(recordField.type)) {
-                    row[id] = this.attributesMapperFactory.getRelationAttributesMapper(recordField.type).mapToInstance(recordField);
-                    return;
-                }
-
-                row[id] = recordField;
+            record.forEach((recordField:Node | Relationship, id) => {
+                row[id] = this.mapRecord(recordField);
             });
 
             return row;
         })
     }
+
+    private mapRecord(recordField:Node | Relationship) {
+        if (_.isArray(recordField)){
+            return recordField.map(el => this.mapRecord(el))
+        }
+
+        if (isNode(recordField) && this.attributesMapperFactory.hasNodeMapper(recordField.labels)) {
+            return this.attributesMapperFactory.getNodeAttributesMapper(recordField.labels).mapToInstance(recordField);
+        }
+
+        if (isRelationship(recordField) && this.attributesMapperFactory.hasRelationMapper(recordField.type)) {
+            return this.attributesMapperFactory.getRelationAttributesMapper(recordField.type).mapToInstance(recordField);
+        }
+
+        return recordField;
+    }
+
+
 }
