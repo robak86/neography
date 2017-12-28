67f2ae9372acf55bd5f5274438931caf462d6377
Tomasz Robaczewski <robak86@gmail.com>
2017-12-28 21:15:30 +0100

    Don't assign unknown properties during entity construction

--- lib/model/AbstractEntity.ts
+++ lib/model/AbstractEntity.ts
@@ -13,10 +13,10 @@ export abstract class AbstractEntity<T> {
         let attributesMetadata = AttributesMetadata.getForInstance(this);
         Object.keys(params).forEach(propertyName => {
             if (!attributesMetadata.isAttributeDefined(propertyName)) {
-                console.warn(`${this.constructor.name} will be initialized with unknown param: ${propertyName}`)
+                console.warn(`${this.constructor.name} is initialized with unknown property: ${propertyName}. This property will be omitted.`)
+            } else {
+                this[propertyName] = params[propertyName as any];
             }
-
-            this[propertyName] = params[propertyName as any];
         });
     }
 
