ed94bc1fd959e574e7bea11c62a68635de0d2d11
Tomasz Robaczewski <robak86@gmail.com>
2017-12-30 15:24:36 +0100

    Remove commented code

--- lib/mappers/AttributesMapper.ts
+++ lib/mappers/AttributesMapper.ts
@@ -18,24 +18,17 @@ export class AttributesMapper<T extends GraphElement> {
         return AttributesMetadata.getForClass(this.klass);
     }
 
-    private get hasAnyAttributes():boolean {
-        return !!this.attributesMetadata;
-    }
-
     mapToInstance(record:Node | Relationship):T {
         if (!isPresent(record)) {
             return record;
         }
 
         let attributesMetadata = this.attributesMetadata;
-
-
-
         let properties = this.readTransformers.reduce((prevVal, transformer) => {
             return transformer.transformInstance(prevVal, attributesMetadata)
         }, record.properties);
 
-        return _.extend(new this.klass(),properties);
+        return _.extend(new this.klass(), properties);
     }
 
     mapToRow(nodeInstance, type:TransformContext) {
@@ -44,28 +37,5 @@ export class AttributesMapper<T extends GraphElement> {
         return this.writeTransformers.reduce((prevVal, transformer) => {
             return transformer.transformRow(prevVal, type, attributesMetadata)
         }, nodeInstance);
-
-
-        // let row = {};
-        //
-        // if (this.hasAnyAttributes) {
-        //     _.forOwn(nodeInstance, (value, propName) => {
-        //         if (this.attributesMetadata.hasAttribute(propName)) { //rewrite only known attributes
-        //             row[propName] = value;
-        //         }
-        //     });
-        // }
-        //
-        // row = this.extensionsRowMapper.mapToRow(row, type);
-        //
-        // if (this.hasAnyAttributes) {
-        //     this.attributesMetadata.forEachAttribute((attributeMetadata, prop) => {
-        //         if (isPresent(row[prop])) {
-        //             row[prop] = attributeMetadata.toRowMapper(row[prop]);
-        //         }
-        //     });
-        // }
-        //
-        // return row;
     }
 }
