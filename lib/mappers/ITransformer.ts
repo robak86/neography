501d52af6ffa5a3d6b6d9d216026c75f06f2cc10
Tomasz Robaczewski <robak86@gmail.com>
2017-12-30 15:24:19 +0100

    Fix isSafeRange

--- lib/mappers/ITransformer.ts
+++ lib/mappers/ITransformer.ts
@@ -4,9 +4,9 @@ import {AttributesMetadata} from "../metadata/AttributesMetadata";
 export type TransformContext = 'create' | 'update' ;
 
 export interface IWriteTransformer {
-    transformRow(row, type:TransformContext, attributesMetadata:AttributesMetadata):Object
+    transformRow(row, type:TransformContext, attributesMetadata:AttributesMetadata|null):Object
 }
 
 export interface IReadTransformer {
-    transformInstance(prev, attributesMetadata:AttributesMetadata)
+    transformInstance(prev, attributesMetadata:AttributesMetadata|null)
 }
