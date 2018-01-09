ec9a5a5ca8336c52be537ecc28e30acd838101b5
Tomasz Robaczewski <robak86@gmail.com>
2018-01-09 23:52:36 +0100

    Add todo to where builder

--- lib/cypher/builders/WhereBuilder.ts
+++ lib/cypher/builders/WhereBuilder.ts
@@ -1,7 +1,12 @@
 import {WhereLiteralQueryPart} from "../match/WhereLiteralQueryPart";
 
-export class WhereBuilder {
+
+
+export class WhereBuilder<T = any> {
     literal(queryPart:string):WhereLiteralQueryPart {
         return new WhereLiteralQueryPart(queryPart)
     }
+
+    //TODO:
+    attribute<K extends keyof T>(prop:T) {} // where(w => [w.attribute('id').in([1,2,3,4,])]
 }
