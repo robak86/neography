d5952130b6af8728c1ea99e2b4653c29a6690c5a
Tomasz Robaczewski <robak86@gmail.com>
2018-01-12 14:34:32 +0100

    Add specs for saving nested hierarchy

--- spec/integration/RelationsBetweenSameNodeTypes.spec.ts
+++ spec/integration/RelationsBetweenSameNodeTypes.spec.ts
@@ -50,5 +50,30 @@ describe(`Relations between the same node types`, () => {
         });
     });
 
+    describe(`saving path`, () => {
+        it(`recursively saves nodes and relationships`, async () => {
+            let p1 = new BlogPostNode({title: 'version1'});
+            let p2 = new BlogPostNode({title: 'version2'});
+            let p3 = new BlogPostNode({title: 'version3'});
+
+            p1.nextRevision.set(p2);
+            p2.nextRevision.set(p3);
+
+            await p1.save();
+
+            let p1Next = await p1.nextRevision.findOne();
+            let p2Prev = await p2.prevRevision.findOne();
+            let p2Next = await p2.nextRevision.findOne();
+            let p3Prev = await p3.prevRevision.findOne();
+
+            expect(await getSharedConnection().nodeQuery(BlogPostNode).count()).to.eq(3);
+
+            expect(p1Next.id).to.eql(p2.id);
+            expect(p2Prev.id).to.eql(p1.id);
+            expect(p2Next.id).to.eql(p3.id);
+            expect(p3Prev.id).to.eql(p2.id);
+        });
+    });
+
 
 });
