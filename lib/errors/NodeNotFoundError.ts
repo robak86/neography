5265afd9ebb8854074481d563549e44db2148547
Tomasz Robaczewski <robak86@gmail.com>
2018-01-12 14:33:52 +0100

    Change error message

--- lib/errors/NodeNotFoundError.ts
+++ lib/errors/NodeNotFoundError.ts
@@ -5,7 +5,7 @@ import {NodeMetadata} from "../metadata/NodeMetadata";
 export class NodeNotFoundError extends Error {
     constructor(nodeClass:Type<NodeEntity<any>>, id?:string) {
         let labels = NodeMetadata.getOrCreateForClass(nodeClass).getLabels().join(':');
-        let message = `Node :${labels}`;
+        let message = `Node ${labels} not found`;
         if (id){
             message += ' with id=${id} not found'
         }
