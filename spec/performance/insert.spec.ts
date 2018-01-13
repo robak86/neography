9f6c0339ad49551546ab6c3524e04bae8d357575
Tomasz Robaczewski <robak86@gmail.com>
2018-01-13 12:26:09 +0100

    Add more performance specs for active record patterns

--- spec/performance/insert.spec.ts
+++ spec/performance/insert.spec.ts
@@ -4,9 +4,8 @@ import {expect} from 'chai';
 import * as _ from 'lodash';
 import {Connection} from "../../lib";
 
-describe("Inserting nodes", () => {
+describe("Inserting nodes @slow", () => {
     beforeEach(async () => await cleanDatabase());
-
 
     const insertNode = (connection) => {
         let node = new DummyGraphNode({attr1: 'someAttr'});
@@ -46,7 +45,7 @@ describe("Inserting nodes", () => {
     describe(`Using simultaneously ${CONNECTIONS_COUNT} connections`, () => {
         let connections:Connection[];
 
-        const getConnection = (idx):Connection => connections[idx%CONNECTIONS_COUNT];
+        const getConnection = (idx):Connection => connections[idx % CONNECTIONS_COUNT];
 
         beforeEach(() => {
             connections = _.times(CONNECTIONS_COUNT).map(() => getDefaultNeography().checkoutConnection());
@@ -61,11 +60,53 @@ describe("Inserting nodes", () => {
 
         it("1000 inserts called in sequence @slow", async () => {
             console.time('sequence inserts');
-            await _.times(1000).reduce((prev:any,current,idx) => {
+            await _.times(1000).reduce((prev:any, current, idx) => {
                 return prev.then(() => insertNode(getConnection(idx)));
             }, Promise.resolve());
             console.timeEnd('sequence inserts');
             expect(await countNodes(getSharedConnection())).to.eq(1000);
+        }).timeout(60000);
+    });
+
+    describe(`using .save method on NodeEntity`, () => {
+        it(`inserts 1000 nodes by calling save at the same time`, async () => {
+            await Promise.all(_.times(1000).map(() => {
+                let node = new DummyGraphNode();
+                return node.save()
+            }));
+
+            expect(await countNodes(getSharedConnection())).to.eq(1000);
+        }).timeout(60000);
+
+        it(`inserts 1000 nodes sequentially`, async () => {
+            await _.times(1000).reduce((prev:any) => {
+                return prev.then(() => {
+                    let node = new DummyGraphNode();
+                    return node.save()
+                });
+            }, Promise.resolve());
+        }).timeout(60000);
+
+        it(`inserts 1000 nodes by calling save at the same time with single connection`, async () => {
+            let connection = getSharedConnection();
+
+            await _.times(1000).reduce((prev:any) => {
+                return prev.then(() => {
+                    let node = new DummyGraphNode();
+                    return node.save(connection)
+                });
+            }, Promise.resolve());
+        }).timeout(60000);
+
+        it(`inserts 1000 nodes sequentially using single connection`, async () => {
+            let connection = getSharedConnection();
+
+            await _.times(1000).reduce((prev:any) => {
+                return prev.then(() => {
+                    let node = new DummyGraphNode();
+                    return node.save(connection)
+                });
+            }, Promise.resolve());
         }).timeout(60000);
     });
 });
