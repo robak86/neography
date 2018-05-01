b74b149addff6ad0c8c53b438675cda70aca9e4a
Tomasz Robaczewski <robak86@gmail.com>
2018-05-01 14:02:32 +0200

    Bump version of typescript and neo4j driver

--- lib/driver/NeoTypes.ts
+++ lib/driver/NeoTypes.ts
@@ -1,8 +1,8 @@
 import neo4j from "neo4j-driver";
 import {Node, Relationship} from "neo4j-driver/types/v1/graph-types";
 
-const Neo4jNode = neo4j.types.Node;
-const Neo4jRelationship = neo4j.types.Relationship;
+const Neo4jNode:any = neo4j.types.Node;
+const Neo4jRelationship:any = neo4j.types.Relationship;
 
 export function isNode(node:any):node is Node {
     return node instanceof Neo4jNode;
